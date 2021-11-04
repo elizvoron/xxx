498aa09c729990eb16cf4d0e79e572f08d816550
elizvoron <91742422+elizvoron@users.noreply.github.com>
2021-11-04 18:31:43 +0400

    Add files via upload

--- face.py
+++ face.py
@@ -4,14 +4,12 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtWidgets import QLabel, QLineEdit, QMainWindow
 from PyQt5.QtGui import QPixmap, QFont
 import sqlite3
-from PyQt5.QtCore import Qt
 
 
 class FirstForm(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-
 
     def initUI(self):
         self.setGeometry(250, 200, 400, 350)
@@ -99,16 +97,15 @@ class FirstForm(QMainWindow):
         self.name = self.name_inputn.text()
         self.sname = self.name_inputsn.text()
         self.fname = self.name_inputfn.text()
-        #fi = str(self.sname + ' ' + self.name + ' ' + self.fname)
-        #con = sqlite3.connect("names.sqlite")
-        #cur = con.cursor()
-        #cur.execute(f'INSERT INTO name(fio, test) VALUES({fi}, {self.test})')
-        #con.commit()
-        #con.close()
-
+        self.fii = str(self.sname + ' ' + self.name + ' ' + self.fname)
+        con = sqlite3.connect("names.sqlite")
+        cur = con.cursor()
+        cur.execute(f'INSERT INTO name(fio) VALUES("{self.fii}")')
+        con.commit()
+        con.close()
 
     def yan(self):
-        self.test = 'Яндекс.Лицей'
+        self.test = str('Яндекс.Лицей')
 
     def mat(self):
         self.test = 'Математика'
@@ -116,12 +113,19 @@ class FirstForm(QMainWindow):
     def nach(self):
         self.test = 'Природа'
 
-    #def keyPressEvent(self, event):
-        #if event.key() == Qt.Key_Enter:
-            #self.second_form = SecondForm()
-            #self.second_form.show()
+    # def keyPressEvent(self, event):
+    # if event.key() == Qt.Key_Enter:
+    # self.second_form = SecondForm()
+    # self.second_form.show()
 
     def open_second_form(self):
+        ts = self.test
+        fi = self.fii
+        con = sqlite3.connect("names.sqlite")
+        cur = con.cursor()
+        cur.execute(f"INSERT INTO tests(test, fi) SELECT '{ts}', id FROM name WHERE fio = '{fi}'")
+        con.commit()
+        con.close()
         self.second_form = SecondForm(self.name, self.test)
         self.second_form.show()
 
@@ -159,11 +163,9 @@ class SecondForm(QWidget):
                 self.quest.append(a[0])
                 self.answ.append(a[-1])
 
-
-
     def initUI(self):
         self.setGeometry(600, 200, 700, 350)
-        self.setWindowTitle('Вторая форма')
+        self.setWindowTitle('ТЕСТ')
         self.lbl = QLabel(self)
         self.lbl.adjustSize()
 
@@ -191,7 +193,7 @@ class SecondForm(QWidget):
         self.label3.adjustSize()
         self.label3.move(40, 60)
 
-        self.btn = QPushButton('NEXT', self)
+        self.btn = QPushButton('Начать!', self)
         font = QFont("Times", 12, QFont.Bold)
         self.btn.setFont(font)
         self.btn.resize(self.btn.sizeHint())
@@ -200,7 +202,7 @@ class SecondForm(QWidget):
 
         self.label4 = QLabel(self)
         self.label4.setFont(QFont("Times", 12, QFont.Bold))
-        self.label4.setText(' ')
+        self.label4.setText('Вопросы:')
         self.label4.adjustSize()
         self.label4.move(40, 80)
 
@@ -212,17 +214,94 @@ class SecondForm(QWidget):
 
         self.ainput = QLineEdit(self)
         self.ainput.adjustSize()
-
         self.ainput.move(100, 100)
 
+        self.btn1 = QPushButton('CLOSE', self)
+        font = QFont("Times", 12, QFont.Bold)
+        self.btn1.setFont(font)
+        self.btn1.resize(self.btn.sizeHint())
+        self.btn1.move(200, 300)
+        self.btn1.clicked.connect(self.open_tr_form)
 
     def questh(self):
-        self.label4.setText(f"{self.quest[self.num]}")
-        self.label4.adjustSize()
-        self.num += 1
-        answ = self.ainput.text()
-        self.ainput.setText(f"")
-        self.answers.append(answ)
+        self.btn.setText('NEXT')
+        self.btn.resize(self.btn.sizeHint())
+        a = self.ainput.text()
+        if not a.isdigit() and a != '':
+            raise ValueError
+        try:
+            if self.num < 10:
+                self.label4.setText(f"{self.quest[self.num]}")
+                self.label4.adjustSize()
+            else:
+                self.label4.setText(f"Всё)")
+                self.label4.adjustSize()
+            self.num += 1
+            self.ainput.setText(f"")
+            self.answers.append(a)
+        except ValueError:
+            self.label4.setText(f"{self.quest[self.num]}")
+            self.label4.adjustSize()
+            self.ainput.setText(f"")
+
+    def open_tr_form(self):
+        self.tr_form = TrForm(self.answers, self.answ)
+        self.tr_form.show()
+
+
+class TrForm(QWidget):
+    def __init__(self, a, b):
+        super().__init__()
+        self.initUI()
+        self.answw = b
+        self.itt = a
+        self.n = 0
+
+    def initUI(self):
+        self.setGeometry(600, 200, 400, 350)
+        self.setWindowTitle('ИТОГ')
+        self.lbl = QLabel(self)
+        self.lbl.adjustSize()
+
+        self.pixmap = QPixmap("фон1")
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(700, 450)
+        self.image.setPixmap(self.pixmap)
+
+        self.label1 = QLabel(self)
+        self.label1.setFont(QFont("Times", 14, QFont.Bold))
+        self.label1.setText('Привет)')
+        self.label1.adjustSize()
+        self.label1.move(40, 20)
+
+        self.label2 = QLabel(self)
+        self.label2.setFont(QFont("Times", 12, QFont.Bold))
+        self.label2.setText(f"Твой результат:")
+        self.label2.adjustSize()
+        self.label2.move(40, 40)
+
+        self.btn = QPushButton('Посмотреть>', self)
+        font = QFont("Times", 10, QFont.Bold)
+        self.btn.setFont(font)
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(40, 60)
+        self.btn.clicked.connect(self.itg)
+
+        self.label2 = QLabel(self)
+        self.label2.setFont(QFont("Times", 15, QFont.Bold))
+        self.label2.setText(f" ")
+        self.label2.adjustSize()
+        self.label2.move(40, 100)
+
+    def itg(self):
+        for i in range(len(self.answw)):
+            if int(self.answw[i]) == int(self.itt[i]):
+                self.n += 1
+        self.n *= 10
+        vv = str(self.n) + '%'
+        self.label2.setText(f"{vv}")
+        self.label2.adjustSize()
 
 
 if __name__ == '__main__':
